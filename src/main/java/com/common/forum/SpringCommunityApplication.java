6353dc436e6d3f74c297abecc3ab86037339ea39
kcuyh <khj879123@gmail.com>
2020-03-09 13:44:49 +0900

    first commit

---
+++ src/main/java/com/common/forum/SpringCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.common.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/common/forum/SpringCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.common.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
