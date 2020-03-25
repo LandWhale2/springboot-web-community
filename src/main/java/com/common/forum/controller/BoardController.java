2816d3235329a99b3ceadaac465d5e8681692285
honaldu <hsh9636@gmail.com>
2020-03-25 20:55:43 +0900

    타임리프 오류 해결

--- src/main/java/com/common/forum/controller/BoardController.java
+++ src/main/java/com/common/forum/controller/BoardController.java
@@ -4,14 +4,12 @@ package com.common.forum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import lombok.AllArgsConstructor;
+import java.util.List;
 
 @Controller
-@AllArgsConstructor
 public class BoardController {
-
-	@GetMapping("/")
-    public String list() {
-		return "ddddd";
-	}
+    @GetMapping("/test")
+    public String test() {
+        return "main.html";
+    }
 }
