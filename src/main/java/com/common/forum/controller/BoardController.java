806c03c8224224bbc53ebf975b1d76d81c3b5e04
kcuyh <khj879123@gmail.com>
2020-04-03 15:01:00 +0900

    [update] memberEntity

--- src/main/java/com/common/forum/controller/BoardController.java
+++ src/main/java/com/common/forum/controller/BoardController.java
@@ -4,13 +4,15 @@ package com.common.forum.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
 @Controller
+@AllArgsConstructor
 public class BoardController {
-	@GetMapping("/test")
+	@GetMapping("/")
     public String test() {
         return "main";
     }
-	
 }
--- src/main/java/com/common/forum/domain/entity/MemberEntity.java
+++ src/main/java/com/common/forum/domain/entity/MemberEntity.java
@@ -1,5 +1,49 @@
 package com.common.forum.domain.entity;
 
-public class MemberEntity {
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "member")
+public class MemberEntity {
+	@Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String email;
+
+    @Column(length = 100, nullable = false)
+    private String password;
+    
+    @Column(length = 10, nullable = false)
+    private String nickname;
+    
+    @Column(name = "ISACTIVE", nullable = false)
+    private boolean isactive = false;
+    
+    @Column(length = 100, nullable = false)
+    private String authkey;
+    
+    
+    @Builder
+    public MemberEntity(Long id, String email, String password, String nickname, boolean isactive, String authkey) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.isactive = isactive;
+        this.authkey = authkey;
+    }
 }
