5594e63a4c91a69ce74abcd74afe203db862df34
kcuyh <khj879123@gmail.com>
2020-04-03 15:18:00 +0900

    member repo ++

--- src/main/java/com/common/forum/domain/repository/MemberRepository.java
+++ src/main/java/com/common/forum/domain/repository/MemberRepository.java
@@ -2,9 +2,11 @@ package com.common.forum.domain.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.common.forum.domain.entity.MemberEntity;
 
-public interface MemberRepository {
+public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
 
 	Optional<MemberEntity> findByEmail(String userEmail);
 
