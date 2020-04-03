d1b11facfd87b223e85d4e5c2c9add56204cd479
kcuyh <khj879123@gmail.com>
2020-04-03 15:17:06 +0900

    member repo

--- src/main/java/com/common/forum/domain/repository/MemberRepository.java
+++ src/main/java/com/common/forum/domain/repository/MemberRepository.java
@@ -1,5 +1,11 @@
 package com.common.forum.domain.repository;
 
+import java.util.Optional;
+
+import com.common.forum.domain.entity.MemberEntity;
+
 public interface MemberRepository {
+
+	Optional<MemberEntity> findByEmail(String userEmail);
 
 }
