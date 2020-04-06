e60f91f0a52a0bf594c6d70f7f4d6295e1bb05ee
kcuyh <khj879123@gmail.com>
2020-04-06 19:27:37 +0900

    login redirect

--- src/main/java/com/common/forum/security/SecurityConfig.java
+++ src/main/java/com/common/forum/security/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
             .and() // 로그인 설정
                                 .formLogin()
                 .loginPage("/user/login")
-                .defaultSuccessUrl("/user/login/result")
+                .defaultSuccessUrl("/")
                 .permitAll()
             .and() // 로그아웃 설정
                                .logout()
