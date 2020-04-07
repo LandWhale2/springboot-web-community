037cbb67d7f67919d5b6ee4dc19c3e5569d093ed
kcuyh <khj879123@gmail.com>
2020-04-07 14:26:58 +0900

    cate repo

---
+++ src/main/java/com/common/forum/domain/repository/CategoryRepository.java
@@ -0,0 +1,17 @@
+package com.common.forum.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.common.forum.domain.entity.CategoryEntity;
+
+
+
+
+
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long> {
+
+	Optional<CategoryEntity> findById(Long categoryId);
+
+}
